fbc9a0e19578a0a56434ce3e2bf5ac96df8a385f
varunp28 <221011463+varunp28@users.noreply.github.com>
2025-09-02 21:25:06 +0530

    Create addition and subtraction.cpp

---
+++ addition and subtraction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=9;
+    int b=5;
+    int*a1;
+    a1=&a;
+    int*b1;
+    b1=&b 
+    int sum,diff;
+    sum=*a1+*b1;
+    diff=*a1-*b1;
+    cout<<"Sum is: "<<sum<<endl;
+    cout<<"Differe+nce is: "<<diff<<endl
+}
+
+//Output:
+//Sum is: 52
+//Differe+nce is: -2
